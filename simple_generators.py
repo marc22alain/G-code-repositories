4712e74ff03fae71e58be575342f55d14c0ac141
marc22alain <marc@marcotypes.ca>
2016-05-13 18:49:45 -0400

    Fixed bug making HCD twice too large

--- simple_generators.py
+++ simple_generators.py
@@ -150,14 +150,17 @@ def bore_tabbed_OD(Z_safe, stock_thickness, cut_per_pass, tab_thickness,
 def polar_holes(Z_safe, stock_thickness, cut_per_pass, target_depth,
               cutter_diameter, circle_diameter, num_holes, hole_circle_diameter):
     assert num_holes > 1, "too few holes to form a circle of holes; must be at least 2"
+
+    hole_circle_radius = hole_circle_diameter / 2.0
+
     file_text = G.set_ABS_mode()
     file_text += G.G0_Z(Z_safe)
 
     radians_increment = 2 * math.pi / num_holes;
 
     # drill first hole
-    x = math.cos(0) * hole_circle_diameter
-    y = math.sin(0) * hole_circle_diameter
+    x = math.cos(0) * hole_circle_radius
+    y = math.sin(0) * hole_circle_radius
     file_text += G.set_INCR_mode()
     file_text += G.G0_XY( (x, y) )
     file_text += bore_circle_ID(Z_safe, stock_thickness, cut_per_pass, target_depth,
@@ -165,8 +168,8 @@ def polar_holes(Z_safe, stock_thickness, cut_per_pass, target_depth,
 
     # drill all other holes
     for i in xrange(1, int(num_holes)):
-        x = - (math.cos((i - 1) * radians_increment) * hole_circle_diameter) + (math.cos(i * radians_increment) * hole_circle_diameter)
-        y = - (math.sin((i - 1) * radians_increment) * hole_circle_diameter) + (math.sin(i * radians_increment) * hole_circle_diameter)
+        x = - (math.cos((i - 1) * radians_increment) * hole_circle_radius) + (math.cos(i * radians_increment) * hole_circle_radius)
+        y = - (math.sin((i - 1) * radians_increment) * hole_circle_radius) + (math.sin(i * radians_increment) * hole_circle_radius)
         file_text += G.set_INCR_mode()
         file_text += G.G0_XY( (x, y) )
         file_text += bore_circle_ID(Z_safe, stock_thickness, cut_per_pass, target_depth,
@@ -176,8 +179,8 @@ def polar_holes(Z_safe, stock_thickness, cut_per_pass, target_depth,
     file_text += G.set_ABS_mode()
     file_text += G.G0_Z(Z_safe)
     file_text += G.set_INCR_mode()
-    x = - (math.cos((num_holes - 1) * radians_increment) * hole_circle_diameter)
-    y = - (math.sin((num_holes - 1) * radians_increment) * hole_circle_diameter)
+    x = - (math.cos((num_holes - 1) * radians_increment) * hole_circle_radius)
+    y = - (math.sin((num_holes - 1) * radians_increment) * hole_circle_radius)
     file_text += G.G0_XY( (x, y) )
 
     return file_text
