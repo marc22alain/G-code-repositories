623213ae2026651da06714f26fa7e32164aa29cb
marc22alain <marc@marcotypes.ca>
2016-05-12 20:58:17 -0400

    Added new bit diameters to defaults

--- MC_defaults.py
+++ MC_defaults.py
@@ -1,7 +1,9 @@
 # here we define the list of bits to choose from:
+#	3.175mm = 1/8"
+#	4.763mm = 3/16"
 #   6.35mm = 1/4"
 #   11.135mm = 7/16"
-bits = (6.35, 11.135)
+bits = (3.175, 4.763, 6.35, 11.135)
 default_safe_Z = 100
 default_feed_rate = 1000
 default_tab_width = 6.35
