38bf34cd503af5a0b57ae1e272b7e8abb7752193
marc22alain <marc@marcotypes.ca>
2016-05-29 20:41:10 -0400

    Refining the geometry classes; trying canvas moving.

--- Amalgamated_Wizard.py
+++ Amalgamated_Wizard.py
@@ -12,8 +12,9 @@ class AmalgamWizard(Frame):
     def __init__(self, master=None):
         self.canvasOptions = {"width":500, "height":500, "bg":"black"}
         Frame.__init__(self, master)
-        self.grid()
+        self.grid()		# TKinter call to arrange the elements
         self.createWidgets()
+        self.wizards = []	# to hold wizards as the user selects them
 
     def createWidgets(self):
         self.CanvasFrame = Frame(self,bd=5)
@@ -27,8 +28,18 @@ class AmalgamWizard(Frame):
         self.plotButton.grid(row=1, column=0, sticky=S, pady=10)
 
     def plotGeometry(self):
-        c = Circle(57, (250, 250), {"outline":"white"})
-        c.draw(self.canvas)
+        c = Circle(57, (0, 0), {"outline":"white"})
+        
+        canvas_items = [c.draw(self.canvas)]
+        for wiz in self.wizards:
+        	geo_entities = wiz.makeGeometricEntities()
+        	for element in geo_entities:
+        		canvas_items.append(element.draw())
+        print canvas_items
+        print self.canvas.bbox(1)
+        # self.canvas.yview_moveto(-250)
+        # self.canvas.yview("moveto", 10000.5)
+        self.canvas.move(1, 250, 250)
 
 
 
--- Canvas_Geometry_classes.py
+++ Canvas_Geometry_classes.py
@@ -4,20 +4,29 @@ from Tkinter import *
 
 
 
+class GeometricEntity(object):
+    """
+    Super-class to underpin all geometric entities.
+    """
+    def __init__(self, line_type):
+        self.line_type = line_type    # is a dict of options to pass in to the canvas
 
-class CanvasGeometry(object):
-    def __init__(self):
-        self.name = joe
+    def draw(self):
+        """
+        Use the standard TKinter Canvas methods.
+        This method MUST return the item's unique ID.
+        """
+        pass
 
 
-class Circle(object):
+class Circle(GeometricEntity):
     def __init__(self, radius, center, line_type):
+        GeometricEntity.__init__(self, line_type)
         self.radius = radius		# is a float
         self.center = center		# is a two-member tuple of floats
-        self.line_type = line_type	# is a dict of options to pass in to the canvas
 
     def draw(self, canvas):
         x = self.center[0]
         y = self.center[1]
         r = self.radius
-        canvas.create_oval(x - r, y - r, x + r, y + r, self.line_type)
+        return canvas.create_oval(x - r, y - r, x + r, y + r, self.line_type, tag="circle")
