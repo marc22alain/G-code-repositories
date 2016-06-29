6bf310173de0dc4288581c809a707d953c25610e
marc22alain <marc@marcotypes.ca>
2016-06-29 10:12:17 -0400

    Adding regression tests for the three wizards.

---
+++ tests/test_hole_borer_wizard.py
@@ -0,0 +1,56 @@
+import os
+import sys
+import unittest
+
+cwd = os.getcwd()
+assert cwd[-6:] == "/tests", "directory locations don't match"
+
+sys.path.append(cwd[0:-6])
+from Hole_Borer import Application
+
+
+Hole_Borer = Application()
+
+class Test_hole_borer_wizard(unittest.TestCase):
+
+
+	def hole_match1(self):
+		return "F1000.0 \nG90 \nG0 Z100.0 \nG91 \nG0 X-4.325 Y0.0 \nG90 \nG0 Z10.0 \nG90 \nG1 Z7.0 \nG91 G17 G2 X0.0 Y0.0 I4.325 J0.0 P1 \nG90 \nG1 Z4.0 \nG91 G17 G2 X0.0 Y0.0 I4.325 J0.0 P1 \nG90 \nG1 Z1.0 \nG91 G17 G2 X0.0 Y0.0 I4.325 J0.0 P1 \nG90 \nG1 Z0.0 \nG91 G17 G2 X0.0 Y0.0 I4.325 J0.0 P1 \nG4 P0.5 \nG90 \nG0 Z100.0 \nG91 \nG0 X4.325 Y0.0 \nG90 \nM2 \n"
+
+
+	def hole_match2(self):
+		return "F1000.0 \nG90 \nG0 Z12.0 \nG1 Z0.0 \nG4 P0.5 \nG0 Z100.0 \nG90 \nM2 \n"
+
+	def test_hole_match1(self):
+		Hole_Borer.feed_rate_var.set("1000")
+		Hole_Borer.Z_safe_var.set("100")
+		Hole_Borer.cut_depth_var.set(3)
+		Hole_Borer.bit_diameter_var.set(6.35)
+		Hole_Borer.stock_thickness_var.set(10)
+
+		Hole_Borer.hole_diameter_var.set(15)
+		Hole_Borer.GenerateCode()
+		g_code = Hole_Borer.g_code
+
+		self.assertEquals(self.hole_match1(), g_code)
+
+
+	def test_hole_match2(self):
+		Hole_Borer.feed_rate_var.set("1000")
+		Hole_Borer.Z_safe_var.set("100")
+		Hole_Borer.cut_depth_var.set(4)
+		Hole_Borer.bit_diameter_var.set(3.175)
+		Hole_Borer.stock_thickness_var.set(12)
+
+		Hole_Borer.hole_diameter_var.set(3.175)
+		Hole_Borer.GenerateCode()
+		g_code = Hole_Borer.g_code
+
+		self.assertEquals(self.hole_match2(), g_code)
+
+
+def hole_borer_wizard_suite():
+    suite = unittest.TestSuite()
+    suite.addTest(Test_hole_borer_wizard("test_hole_match1"))
+    suite.addTest(Test_hole_borer_wizard("test_hole_match2"))
+    return suite
