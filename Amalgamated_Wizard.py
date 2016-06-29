50907c353c1f71471cb4cece4eaca419bfd2e73a
marc22alain <marc@marcotypes.ca>
2016-06-28 23:32:42 -0400

    Elaborating the AmalgamWizard, now doing something

--- Amalgamated_Wizard.py
+++ Amalgamated_Wizard.py
@@ -3,6 +3,8 @@
 from Tkinter import *
 import os
 from Canvas_Geometry_classes import *
+from Doughnut_Cutter_class import DoughnutCutter
+from Setup_class import Setup
 
 
 IN_AXIS = os.environ.has_key("AXIS_PROGRESS_BAR")
@@ -13,8 +15,9 @@ class AmalgamWizard(Frame):
         self.canvasOptions = {"width":500, "height":500, "bg":"black"}
         Frame.__init__(self, master)
         self.grid()		# TKinter call to arrange the elements
+        self.wizards = []   # to hold wizards as the user selects them
         self.createWidgets()
-        self.wizards = []	# to hold wizards as the user selects them
+        self.drawGrid()
 
     def createWidgets(self):
         self.CanvasFrame = Frame(self,bd=5)
@@ -27,6 +30,13 @@ class AmalgamWizard(Frame):
         self.plotButton = Button(self, text='Plot all geometry', command=self.plotGeometry)
         self.plotButton.grid(row=1, column=0, sticky=S, pady=10)
 
+        self.EntryFrame = Frame(self,bd=5)
+        self.EntryFrame.grid(row=0, column=1)
+
+        self.setup_wizard = Setup()
+        self.doughnut_wizard = DoughnutCutter(self.EntryFrame, self.setup_wizard)
+        self.wizards.append(self.doughnut_wizard)
+
     def plotGeometry(self):
         c = Circle(57, (0, 0), {"outline":"white"})
         
@@ -34,14 +44,21 @@ class AmalgamWizard(Frame):
         for wiz in self.wizards:
         	geo_entities = wiz.makeGeometricEntities()
         	for element in geo_entities:
-        		canvas_items.append(element.draw())
+        		canvas_items.append(element.draw(self.canvas))
         print canvas_items
         print self.canvas.bbox(1)
         # self.canvas.yview_moveto(-250)
         # self.canvas.yview("moveto", 10000.5)
-        self.canvas.move(1, 250, 250)
+        self.canvas.config(scrollregion=self.canvas.bbox(ALL))
+        # self.canvas.move(1, 250, 250)
 
-
+    def drawGrid(self):
+        x_range = self.canvasOptions["width"] / 2
+        y_range = self.canvasOptions["height"] / 2
+        self.canvas.create_line( - x_range, 0, x_range, 0, tag="grid")
+        self.canvas.create_line( 0, - y_range, 0, y_range, tag="grid")
+        self.canvas.itemconfig("grid", fill="#555")
+        self.canvas.config(scrollregion=self.canvas.bbox(ALL))
 
 
 
