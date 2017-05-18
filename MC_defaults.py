c52222fa8e5437cf7706e2b97c06701eef702f34
marc22alain <marc@marcotypes.ca>
2017-05-17 23:32:23 -0400

    Adding more bits to defaults

--- MC_defaults.py
+++ MC_defaults.py
@@ -1,11 +1,13 @@
 # here we define the list of bits to choose from:
-#	3.175mm = 1/8"
-#	4.763mm = 3/16"
+#   3.175mm = 1/8"
+#   4.763mm = 3/16"
 #   6.35mm = 1/4"
+#   9.525mm = 3/8"
 #   11.135mm = 7/16"
-#	19.05mm = 3/4"
-bits = (3.175, 4.763, 6.35, 11.135, 19.05)
-ball_noses = (12.7, 19.05)
+#   12.7mm = 1/2"
+#   19.05mm = 3/4"
+bits = (3.175, 4.763, 6.35, 9.525, 11.135, 12.7, 19.05)
+ball_noses = (9.525, 12.7, 19.05)
 default_safe_Z = 100
 default_feed_rate = 1000
 default_tab_width = 6.35
