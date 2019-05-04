af18efd13c7e444bdc55efbd0f2462376991474f
Marc de Niverville <marc22alain@users.noreply.github.com>
2019-05-04 16:43:30 -0400

    implements simple machine and simple workpiece

--- machines/SimpleMachine_class.py
+++ machines/SimpleMachine_class.py
@@ -1,3 +1,26 @@
+from option_queries import *
+
 class SimpleMachine(object):
+    name = 'Simple Machine'
+    option_query_classes = [
+        BitDiameterQuery,
+        SafeZQuery,
+        FeedRateQuery
+    ]
+
     def __init__(self):
-        pass
+        self.option_queries = {}
+
+    def getOptionQueries(self):
+        if len(self.option_queries.keys()) == 0:
+            print 'instantiating new Query objects'
+            self.option_queries = { key: key() for key in self.option_query_classes }
+        return self.option_queries
+
+    def getParams(self):
+        params = {
+            'bit_diameter': self.option_queries[BitDiameterQuery].getValue(),
+            'safe_z': self.option_queries[SafeZQuery].getValue(),
+            'feed_rate': self.option_queries[FeedRateQuery].getValue()
+        }
+        return params
--- workpieces/SimpleWorkpiece_class.py
+++ workpieces/SimpleWorkpiece_class.py
@@ -1,3 +1,25 @@
+from option_queries import *
+
 class SimpleWorkpiece(object):
+    name = 'Simple Workpiece'
+    option_query_classes = [
+        StockLengthQuery,
+        StockWidthQuery,
+        StockHeightQuery
+    ]
+
     def __init__(self):
-        pass
+        self.option_queries = {}
+
+    def getOptionQueries(self):
+        if len(self.option_queries.keys()) == 0:
+            self.option_queries = { key: key() for key in self.option_query_classes }
+        return self.option_queries
+
+    def getParams(self):
+        params = {
+            'stock_length': self.option_queries[StockLengthQuery].getValue(),
+            'stock_width': self.option_queries[StockWidthQuery].getValue(),
+            'stock_height': self.option_queries[StockHeightQuery].getValue()
+        }
+        return params
