e7bfa4ad69c9d9fbfa7adc642a1cdf33a539d9ce
Marc de Niverville <marc22alain@users.noreply.github.com>
2019-06-18 22:15:58 -0400

    updates ODCircularGroove for new drawing method

--- drawn_features/__init__.py
+++ drawn_features/__init__.py
@@ -3,3 +3,4 @@ from FeatureDrawing_class import FeatureDrawing
 from HoleDrawing_class import HoleDrawing
 from LinearDistributionDrawing_class import LinearDistributionDrawing
 from RoundedRectangleDrawing_class import RoundedRectangleDrawing
+from ODCircularGrooveDrawing_class import ODCircularGrooveDrawing
