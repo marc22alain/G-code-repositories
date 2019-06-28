8f82c18e33733724f0e5f602a79453b6d6615ed9
Marc de Niverville <marc22alain@users.noreply.github.com>
2019-06-28 14:59:35 -0400

    simplifies CircularGrooveDrawing

--- drawn_features/CircularGrooveDrawing_class.py
+++ drawn_features/CircularGrooveDrawing_class.py
@@ -10,66 +10,54 @@ class CircularGrooveDrawing(FeatureDrawing, AutoObserver):
         FeatureDrawing.__init__(self)
 
     def _drawXYentities(self):
+        plane = 'XY'
         options = {"tag":"geometry","outline":"yellow","fill":None}
         refX = self.params['refX']
         refY = self.params['refY']
         radius = self.params['diameter'] /2
         bit_radius = self.params['bit_diameter'] / 2
-        if len(self.entities['XY']) == 0:
-            self.entities['XY'].append(Circle(self.view_space).setAllByCenterRadius((refX, refY, radius - bit_radius), options).draw())
-            self.entities['XY'].append(Circle(self.view_space).setAllByCenterRadius((refX, refY, radius + bit_radius), options).draw())
-        else:
-            self.entities['XY'][0].setAllByCenterRadius((refX, refY, radius - bit_radius), options).draw()
-            self.entities['XY'][1].setAllByCenterRadius((refX, refY, radius + bit_radius), options).draw()
+        if len(self.entities[plane]) == 0:
+            self.entities[plane].append(Circle(self.view_space))
+            self.entities[plane].append(Circle(self.view_space))
+        self.entities[plane][0].setAllByCenterRadius((refX, refY, radius - bit_radius), options).draw()
+        self.entities[plane][1].setAllByCenterRadius((refX, refY, radius + bit_radius), options).draw()
 
     def _drawYZentities(self):
+        plane = 'YZ'
         options = {"tag":"geometry","outline":"yellow","fill":None}
         cut_depth = self.params['cut_depth']
         refY = self.params['refY']
         radius = self.params['diameter'] /2
         bit_radius = self.params['bit_diameter'] / 2
         stock_height = self.params['stock_height']
-        if len(self.entities['YZ']) == 0:
-            self.entities['YZ'].append(Rectangle(self.view_space).setAll(
-                (refY - radius - bit_radius, stock_height - cut_depth, refY + radius + bit_radius, stock_height),
-                options
-            ).draw())
-            self.entities['YZ'].append(Rectangle(self.view_space).setAll(
-                (refY - radius + bit_radius, stock_height - cut_depth, refY + radius - bit_radius, stock_height),
-                options
-            ).draw())
-        else:
-            self.entities['YZ'][0].setAll(
-                (refY - radius - bit_radius, stock_height - cut_depth, refY + radius + bit_radius, stock_height),
-                options
-            ).draw()
-            self.entities['YZ'][1].setAll(
-                (refY - radius + bit_radius, stock_height - cut_depth, refY + radius - bit_radius, stock_height),
-                options
-            ).draw()
+        if len(self.entities[plane]) == 0:
+            self.entities[plane].append(Rectangle(self.view_space))
+            self.entities[plane].append(Rectangle(self.view_space))
+        self.entities[plane][0].setAll(
+            (refY - radius - bit_radius, stock_height - cut_depth, refY + radius + bit_radius, stock_height),
+            options
+        ).draw()
+        self.entities[plane][1].setAll(
+            (refY - radius + bit_radius, stock_height - cut_depth, refY + radius - bit_radius, stock_height),
+            options
+        ).draw()
 
     def _drawXZentities(self):
+        plane = 'XZ'
         options = {"tag":"geometry","outline":"yellow","fill":None}
         cut_depth = self.params['cut_depth']
         refX = self.params['refX']
         radius = self.params['diameter'] /2
         bit_radius = self.params['bit_diameter'] / 2
         stock_height = self.params['stock_height']
-        if len(self.entities['XZ']) == 0:
-            self.entities['XZ'].append(Rectangle(self.view_space).setAll(
-                (refX - radius - bit_radius, stock_height - cut_depth, refX + radius + bit_radius, stock_height),
-                options
-            ).draw())
-            self.entities['XZ'].append(Rectangle(self.view_space).setAll(
-                (refX - radius + bit_radius, stock_height - cut_depth, refX + radius - bit_radius, stock_height),
-                options
-            ).draw())
-        else:
-            self.entities['XZ'][0].setAll(
-                (refX - radius - bit_radius, stock_height - cut_depth, refX + radius + bit_radius, stock_height),
-                options
-            ).draw()
-            self.entities['XZ'][1].setAll(
-                (refX - radius + bit_radius, stock_height - cut_depth, refX + radius - bit_radius, stock_height),
-                options
-            ).draw()
+        if len(self.entities[plane]) == 0:
+            self.entities[plane].append(Rectangle(self.view_space))
+            self.entities[plane].append(Rectangle(self.view_space))
+        self.entities[plane][0].setAll(
+            (refX - radius - bit_radius, stock_height - cut_depth, refX + radius + bit_radius, stock_height),
+            options
+        ).draw()
+        self.entities[plane][1].setAll(
+            (refX - radius + bit_radius, stock_height - cut_depth, refX + radius - bit_radius, stock_height),
+            options
+        ).draw()
