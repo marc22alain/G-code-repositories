586f907ef10830f20b0232b43e9f1d4b04f43fdb
Marc de Niverville <marc22alain@users.noreply.github.com>
2019-06-28 16:00:34 -0400

    simplifies RectangularGrooveDrawing

--- drawn_features/RectangularGrooveDrawing_class.py
+++ drawn_features/RectangularGrooveDrawing_class.py
@@ -7,7 +7,6 @@ from errors import *
 # used by RectangularGroove
 class RectangularGrooveDrawing(FeatureDrawing, AutoObserver):
     '''
-    This is a sausage.
     The drawing's reference point is one of ['center', 'lower-left']
     'center' is at the very center of the drawing.
     'lower-left' is at the centerpoint of the left-side radius.
@@ -20,6 +19,7 @@ class RectangularGrooveDrawing(FeatureDrawing, AutoObserver):
         log('RectangularGrooveDrawing __init__')
 
     def _drawXYentities(self):
+        plane = 'XY'
         options = {"tag":"geometry","outline":"yellow","fill":None}
         cut_depth = self.params['cut_depth']
         refX = self.params['refX']
@@ -29,50 +29,32 @@ class RectangularGrooveDrawing(FeatureDrawing, AutoObserver):
         half_side_x = side_X / 2
         half_side_y = side_Y / 2
         bit_radius = self.params['bit_diameter'] / 2
-        if len(self.entities['XY']) == 0:
-            if self.reference_point == 'center':
-                self.entities['XY'].append(RoundedRectangle(self.view_space).setAll(
-                    (refX - half_side_x - bit_radius, refY - half_side_y - bit_radius, refX + half_side_x + bit_radius, refY + half_side_y + bit_radius, bit_radius),
-                    options
-                ).draw())
-                self.entities['XY'].append(Rectangle(self.view_space).setAll(
-                    (refX - half_side_x + bit_radius, refY - half_side_y + bit_radius, refX + half_side_x - bit_radius, refY + half_side_y - bit_radius),
-                    options
-                ).draw())
-            elif self.reference_point == 'lower-left':
-                self.entities['XY'].append(RoundedRectangle(self.view_space).setAll(
-                    (refX - bit_radius, refY - bit_radius, refX + side_X + bit_radius, refY + side_Y + bit_radius, bit_radius),
-                    options
-                ).draw())
-                self.entities['XY'].append(Rectangle(self.view_space).setAll(
-                    (refX + bit_radius, refY + bit_radius, refX + side_X - bit_radius, refY + side_Y - bit_radius),
-                    options
-                ).draw())
-            else:
-                raise ReferencePointError(self, self.reference_point)
+        if len(self.entities[plane]) == 0:
+            self.entities[plane].append(RoundedRectangle(self.view_space))
+            self.entities[plane].append(Rectangle(self.view_space))
+        if self.reference_point == 'center':
+            self.entities[plane][0].setAll(
+                (refX - half_side_x - bit_radius, refY - half_side_y - bit_radius, refX + half_side_x + bit_radius, refY + half_side_y + bit_radius, bit_radius),
+                options
+            ).draw()
+            self.entities[plane][1].setAll(
+                (refX - half_side_x + bit_radius, refY - half_side_y + bit_radius, refX + half_side_x - bit_radius, refY + half_side_y - bit_radius),
+                options
+            ).draw()
+        elif self.reference_point == 'lower-left':
+            self.entities[plane][0].setAll(
+                (refX - bit_radius, refY - bit_radius, refX + side_X + bit_radius, refY + side_Y + bit_radius, bit_radius),
+                options
+            ).draw()
+            self.entities[plane][1].setAll(
+                (refX + bit_radius, refY + bit_radius, refX + side_X - bit_radius, refY + side_Y - bit_radius),
+                options
+            ).draw()
         else:
-            if self.reference_point == 'center':
-                self.entities['XY'][0].setAll(
-                    (refX - half_side_x - bit_radius, refY - half_side_y - bit_radius, refX + half_side_x + bit_radius, refY + half_side_y + bit_radius, bit_radius),
-                    options
-                ).draw()
-                self.entities['XY'][1].setAll(
-                    (refX - half_side_x + bit_radius, refY - half_side_y + bit_radius, refX + half_side_x - bit_radius, refY + half_side_y - bit_radius),
-                    options
-                ).draw()
-            elif self.reference_point == 'lower-left':
-                self.entities['XY'][0].setAll(
-                    (refX - bit_radius, refY - bit_radius, refX + side_X + bit_radius, refY + side_Y + bit_radius, bit_radius),
-                    options
-                ).draw()
-                self.entities['XY'][1].setAll(
-                    (refX + bit_radius, refY + bit_radius, refX + side_X - bit_radius, refY + side_Y - bit_radius),
-                    options
-                ).draw()
-            else:
-                raise ReferencePointError(self, self.reference_point)
+            raise ReferencePointError(self, self.reference_point)
 
     def _drawYZentities(self):
+        plane = 'YZ'
         options = {"tag":"geometry","outline":"yellow","fill":None}
         cut_depth = self.params['cut_depth']
         refY = self.params['refY']
@@ -82,50 +64,32 @@ class RectangularGrooveDrawing(FeatureDrawing, AutoObserver):
         half_side_y = side_Y / 2
         bit_radius = self.params['bit_diameter'] / 2
         stock_height = self.params['stock_height']
-        if len(self.entities['YZ']) == 0:
-            if self.reference_point == 'center':
-                self.entities['YZ'].append(Rectangle(self.view_space).setAll(
-                    (refY - half_side_y - bit_radius, stock_height - cut_depth, refY + half_side_y + bit_radius, stock_height),
-                    options
-                ).draw())
-                self.entities['YZ'].append(Rectangle(self.view_space).setAll(
-                    (refY - half_side_y + bit_radius, stock_height - cut_depth, refY + half_side_y - bit_radius, stock_height),
-                    options
-                ).draw())
-            elif self.reference_point == 'lower-left':
-                self.entities['YZ'].append(Rectangle(self.view_space).setAll(
-                    (refY - bit_radius, stock_height - cut_depth, refY + side_Y + bit_radius, stock_height),
-                    options
-                ).draw())
-                self.entities['YZ'].append(Rectangle(self.view_space).setAll(
-                    (refY + bit_radius, stock_height - cut_depth, refY + side_Y - bit_radius, stock_height),
-                    options
-                ).draw())
-            else:
-                raise ReferencePointError(self, self.reference_point)
+        if len(self.entities[plane]) == 0:
+            self.entities[plane].append(Rectangle(self.view_space))
+            self.entities[plane].append(Rectangle(self.view_space))
+        if self.reference_point == 'center':
+            self.entities[plane][0].setAll(
+                (refY - half_side_y - bit_radius, stock_height - cut_depth, refY + half_side_y + bit_radius, stock_height),
+                options
+            ).draw()
+            self.entities[plane][1].setAll(
+                (refY - half_side_y + bit_radius, stock_height - cut_depth, refY + half_side_y - bit_radius, stock_height),
+                options
+            ).draw()
+        elif self.reference_point == 'lower-left':
+            self.entities[plane][0].setAll(
+                (refY - bit_radius, stock_height - cut_depth, refY + side_Y + bit_radius, stock_height),
+                options
+            ).draw()
+            self.entities[plane][1].setAll(
+                (refY + bit_radius, stock_height - cut_depth, refY + side_Y - bit_radius, stock_height),
+                options
+            ).draw()
         else:
-            if self.reference_point == 'center':
-                self.entities['YZ'][0].setAll(
-                    (refY - half_side_y - bit_radius, stock_height - cut_depth, refY + half_side_y + bit_radius, stock_height),
-                    options
-                ).draw()
-                self.entities['YZ'][1].setAll(
-                    (refY - half_side_y + bit_radius, stock_height - cut_depth, refY + half_side_y - bit_radius, stock_height),
-                    options
-                ).draw()
-            elif self.reference_point == 'lower-left':
-                self.entities['YZ'][0].setAll(
-                    (refY - bit_radius, stock_height - cut_depth, refY + side_Y + bit_radius, stock_height),
-                    options
-                ).draw()
-                self.entities['YZ'][1].setAll(
-                    (refY + bit_radius, stock_height - cut_depth, refY + side_Y - bit_radius, stock_height),
-                    options
-                ).draw()
-            else:
-                raise ReferencePointError(self, self.reference_point)
+            raise ReferencePointError(self, self.reference_point)
 
     def _drawXZentities(self):
+        plane = 'XZ'
         options = {"tag":"geometry","outline":"yellow","fill":None}
         cut_depth = self.params['cut_depth']
         refX = self.params['refX']
@@ -135,45 +99,26 @@ class RectangularGrooveDrawing(FeatureDrawing, AutoObserver):
         half_side_y = side_Y / 2
         bit_radius = self.params['bit_diameter'] / 2
         stock_height = self.params['stock_height']
-        if len(self.entities['XZ']) == 0:
-            if self.reference_point == 'center':
-                self.entities['XZ'].append(Rectangle(self.view_space).setAll(
-                    (refX - half_side_x - bit_radius, stock_height - cut_depth, refX + half_side_x + bit_radius, stock_height),
-                    options
-                ).draw())
-                self.entities['XZ'].append(Rectangle(self.view_space).setAll(
-                    (refX - half_side_x + bit_radius, stock_height - cut_depth, refX + half_side_x - bit_radius, stock_height),
-                    options
-                ).draw())
-            elif self.reference_point == 'lower-left':
-                self.entities['XZ'].append(Rectangle(self.view_space).setAll(
-                    (refX - bit_radius, stock_height - cut_depth, refX + side_X + bit_radius, stock_height),
-                    options
-                ).draw())
-                self.entities['XZ'].append(Rectangle(self.view_space).setAll(
-                    (refX + bit_radius, stock_height - cut_depth, refX + side_X - bit_radius, stock_height),
-                    options
-                ).draw())
-            else:
-                raise ReferencePointError(self, self.reference_point)
+        if len(self.entities[plane]) == 0:
+            self.entities[plane].append(Rectangle(self.view_space))
+            self.entities[plane].append(Rectangle(self.view_space))
+        if self.reference_point == 'center':
+            self.entities[plane][0].setAll(
+                (refX - half_side_x - bit_radius, stock_height - cut_depth, refX + half_side_x + bit_radius, stock_height),
+                options
+            ).draw()
+            self.entities[plane][1].setAll(
+                (refX - half_side_x + bit_radius, stock_height - cut_depth, refX + half_side_x - bit_radius, stock_height),
+                options
+            ).draw()
+        elif self.reference_point == 'lower-left':
+            self.entities[plane][0].setAll(
+                (refX - bit_radius, stock_height - cut_depth, refX + side_X + bit_radius, stock_height),
+                options
+            ).draw()
+            self.entities[plane][1].setAll(
+                (refX + bit_radius, stock_height - cut_depth, refX + side_X - bit_radius, stock_height),
+                options
+            ).draw()
         else:
-            if self.reference_point == 'center':
-                self.entities['XZ'][0].setAll(
-                    (refX - half_side_x - bit_radius, stock_height - cut_depth, refX + half_side_x + bit_radius, stock_height),
-                    options
-                ).draw()
-                self.entities['XZ'][1].setAll(
-                    (refX - half_side_x + bit_radius, stock_height - cut_depth, refX + half_side_x - bit_radius, stock_height),
-                    options
-                ).draw()
-            elif self.reference_point == 'lower-left':
-                self.entities['XZ'][0].setAll(
-                    (refX - bit_radius, stock_height - cut_depth, refX + side_X + bit_radius, stock_height),
-                    options
-                ).draw()
-                self.entities['XZ'][1].setAll(
-                    (refX + bit_radius, stock_height - cut_depth, refX + side_X - bit_radius, stock_height),
-                    options
-                ).draw()
-            else:
-                raise ReferencePointError(self, self.reference_point)
+            raise ReferencePointError(self, self.reference_point)
