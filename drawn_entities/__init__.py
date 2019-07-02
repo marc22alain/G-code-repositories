39e7e3472eeb78f8bcac5ec9444df014fb1a59e6
Marc de Niverville <marc22alain@users.noreply.github.com>
2019-07-01 22:31:33 -0400

    removes DuplicateEntity from the project

--- drawn_entities/__init__.py
+++ drawn_entities/__init__.py
@@ -3,4 +3,3 @@ from Circle_class import Circle
 from Line_class import Line
 from Rectangle_class import Rectangle
 from RoundedRectangle_class import RoundedRectangle
-from DuplicateEntity_class import DuplicateEntity
