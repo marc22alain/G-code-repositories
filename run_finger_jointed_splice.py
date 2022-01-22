fa7d0fb7b9b7f1752d0ac49a67484df6f7f3fbd1
Marc de Niverville <marc22alain@users.noreply.github.com>
2022-01-22 11:29:19 -0500

    updates run script

--- run_finger_jointed_splice.py
+++ run_finger_jointed_splice.py
@@ -5,17 +5,35 @@ machine_params = {
     'safe_z': 80,
     'feed_rate': 1000
 }
-
+"""
+# for good splices
 workpiece_params = {
     # 'stock_length': 0, ... not strictly required
     'stock_width': 41.275,  # 1.625"
     'stock_height': 7.9375  # 5/16 ""
 }
 
+# 'fit_factor': .175 somewhat splits the difference between top and bottom
+# .2mm to .5mm difference total width
 cutting_params = {
     'cut_per_pass': 4,
-    'finger_depth': 20,
-    'fit_factor': 0
+    'finger_depth': 20,  # includes the radii at both ends
+    'fit_factor': - .175
+}
+"""
+
+# for hidden finger joint
+# get the hiding feature by setting Z zero at 2mm above the actual table
+workpiece_params = {
+    # 'stock_length': 0, ... not strictly required
+    'stock_width': 20,  # 1.625"
+    'stock_height': 19.2  # 5/16 ""
+}
+
+cutting_params = {
+    'cut_per_pass': 4,
+    'finger_depth': 26.35,  # includes the radii at both ends
+    'fit_factor': - .175  # starting with the fit factor that works for splices
 }
 
 f = FingerJointedSplice(
